352a5a9116c07855481bec107e8fa935084e9749
V VIVEKANANDA <vivekananda24@gmail.com>
2026-07-28 19:52:49 +0530

    .

--- server/app.js
+++ server/app.js
@@ -9,7 +9,12 @@ const upload = multer({ storage: multer.memoryStorage() });
 app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  // Respond to preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
   next();
 });
 
